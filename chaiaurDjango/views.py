6221a688a3554556f54857ecc7a9cd826abe44c1
Abhinav Bahadur Singh <144931517+abhinavbahadursingh@users.noreply.github.com>
2024-11-20 09:26:44 +0530

    basic settings and url linking response

---
+++ chaiaurDjango/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    # return HttpResponse("Home page")
+    return render(request , 'index.html')
+def contact(request):
+    return HttpResponse("contact page")
+def about(request):
+    return HttpResponse("about page")
+def login(request):
+    return HttpResponse("login page")
